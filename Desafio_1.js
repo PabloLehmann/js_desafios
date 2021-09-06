6751a331f9be3b95ef81aed5b0cf90666123bb70
Pablo Lehmann <dantelehmann354@gmail.com>
2021-09-06 19:49:17 -0300

    Repositorio desafios

---
+++ Desafio_1.js
@@ -0,0 +1,7 @@
+//Desafio 1
+
+var n1 = parseInt(prompt ("Ingrese un numero"));
+var n2 = parseInt(prompt ("Ingrese otro numero"));
+var resultado = n1 + n2;
+console.log ("El resultado de la suma de sus dos numeros es: " + resultado);
+
---
+++ desafio_2.js
@@ -0,0 +1,7 @@
+var edad1 = parseInt(prompt("Ingrese la edad de su papá"));
+var edad2 = parseInt(prompt("Ingrese la edad de su mamá"));
+if (edad1 < edad2){
+    alert ("Su mamá es mas grande que su papá")
+}else{
+    alert ("Su papá es mas grande que su mamá")
+}
