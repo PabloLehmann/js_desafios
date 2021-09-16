befa4c4912aa487f1f9dc19bcb78e4955dedf31e
Pablo Lehmann <dantelehmann354@gmail.com>
2021-09-15 21:39:11 -0300

    Desafio complementario

--- desafio_4/index.js
+++ desafio_4/index.js
@@ -1,9 +1,15 @@
 function porcentajeDeMercaderia(pPregunta){
-    let n1 = prompt(pPregunta);
-    let n2 = parseInt(n1);
-    return n2;
+    let n1 = parseInt(prompt(pPregunta));
+    return n1;
 }
 var monto = porcentajeDeMercaderia ("Ingrese el valor de la mercaderia");
 var kilos = porcentajeDeMercaderia ("Ingrese el kilaje de la mercaderia");
 var precio= ((monto/2) + monto)/kilos;
-alert("Este es el precio: $" + precio + " para el publico")
+alert("Este es el precio: $" + precio + " para el publico");
+var iva = prompt ("deseas agregarle el IVA a la mercaderia? SI/NO");
+if (iva == "SI"){
+    var ivaAgreg = precio * 1.21;
+    alert("El precio con el iva agregado es: " + ivaAgreg);
+}else{
+    alert("Ok");
+}
