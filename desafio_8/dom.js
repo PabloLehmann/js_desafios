70f72307d6325661de9b1e8edcd32dbf0e06880c
PabloLehmann <88408538+PabloLehmann@users.noreply.github.com>
2021-10-05 00:00:42 -0300

    Saque lo comentado

--- desafio_8/dom.js
+++ desafio_8/dom.js
@@ -1,27 +1,3 @@
-/* function porcentajeDeMercaderia(pPregunta){
-    let n1 = prompt(pPregunta);
-    let n2 = parseInt(n1);
-    return n2;
-}
-var monto = porcentajeDeMercaderia ("Ingrese el valor de la mercaderia");
-var kilos = porcentajeDeMercaderia ("Ingrese el kilaje de la mercaderia");
-var precio= ((monto/2) + monto)/kilos;
-alert("Este es el precio: $" + precio + " para el publico") */
-
-
-
-/* var monto = document.getElementById("mercaderia")
-var kilos = porcentajeDeMercaderia ("Ingrese el kilaje de la mercaderia");
-var precio= ((monto/2) + monto)/kilos;
-alert("Este es el precio: $" + precio + " para el publico") */
-
-/* const pregunt1= prompt("Ingrese el valor")
-const preg1= document.getElementsByTagName('h3')
-
-preg1.texContent= prompt("Ingrese el valor")
- */
-
-///////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////
 
 const titulo= document.getElementsByTagName("h1")[0]
@@ -45,21 +21,4 @@ for(let li of listaMercaderia){
 }
 
 //////////////////////////////////////////////////////////////
-
-/* const input = document.getElementById('input')
-input.textContent = 
- console.log(input.value) */
-/*
-const pokemones = ["pikachu", "charmander", "bulbasaur", "squirtle", "charizard", "evee", "raichu", "snorlax"]
- */
-/* const select = document.getElementById('pokemon')
-
-pokemones.forEach( (pokemon) => {
-    const option = document.createElement('option')
-    option.value = pokemon
-    option.innerHTML = pokemon
-
-    select.appendChild(option)
-}) */
-
 
