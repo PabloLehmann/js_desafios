f0ee9bd11ac006524bf81ce9f5d50760fcbd5d07
Pablo Lehmann <dantelehmann354@gmail.com>
2021-10-20 21:09:42 -0300

    Desafio animaciones

---
+++ entregasProyecto/animaciones.js
@@ -0,0 +1,38 @@
+
+$('#fadein').click (()=>{
+    $('#listaDeClientes').fadeIn(1000)
+    })
+    $('#fadeout').click (()=>{
+        $('#listaDeClientes').fadeOut(1000)
+    })
+    //////////////////////////////////////
+    $("#p1").css("color", "red")
+            .css("font-size"," xx-large")
+            .slideUp(2000)
+            .slideDown(2000)
+    
+            
+    
+    
+    /////////////////////////////////
+    $("#body").css("background","aquamarine")
+              
+              .fadeIn(1000)
+              .fadeOut(1000)
+              .fadeIn(1000)
+              
+              .css("font-zide","700")
+
+    $("#centrar").css("background"," rgb(184, 233, 217)")
+                 .css("align-items", "center")     
+                 .css("border", "solid")
+                 .css("height", "150px")
+                 .css("width", "350px"),
+                 
+   /////////////////////////////////////
+   $(".titulo").animate({  left:'250px',
+                        height:'70px',
+                        width:'250px'   }, 
+                         "5000"           );
+              
+              
--- entregasProyecto/jquery.js
+++ entregasProyecto/jquery.js
@@ -3,7 +3,7 @@ parrafo.html('<h2>Ingresar mercaderia a stock?</h2>')
 ///////////////////////////
 
 $('#boton').on('click', ()=> {
-
+$("#myForm").remove()
 $("body").append(`<form id="myForm">
                        <input type="text" id="mercaderia" >
                        <input type="number" id="kilos">
@@ -15,8 +15,10 @@ $("#boton2").on("click", (e) => {
     let mercaderia = $("#mercaderia").val()
     let kilos = $("#kilos").val()
     const nombreStock = $("#nombreStock")
+    nombreStock.html("")
     nombreStock.html("La mercaderia ingresada a stok es: "+mercaderia + " con la cantidad de kilos de : " + kilos);
     
 });
 
 })
+//////////////////////////////////////////
